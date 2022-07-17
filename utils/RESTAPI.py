0d0927be82f58878d43a9d87337a5e7ee70b83f2
Qianyiovo <1291525582@qq.com>
2022-07-17 21:28:51 +0800

    v1.4.0

--- utils/RESTAPI.py
+++ utils/RESTAPI.py
@@ -46,3 +46,48 @@ class V3:
                     return False, response.json()
             else:
                 return False, "无法连接至服务器"
+
+
+class Player:
+    @staticmethod
+    def inventory(ip: str, port: str, token: str, name: str):
+        """
+        获取指定玩家的库存
+        :param ip: 服务器ip
+        :param port: 服务器端口
+        :param token: 服务器令牌
+        :param name: 玩家名称
+        :return: 执行结果 成功返回[True, 执行结果] 失败返回[False, 执行结果/失败原因]
+        """
+        url = f"http://{ip}:{port}/player/inventory?player={name}&token={token}"
+        headers = {"Accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            if response.json()["status"] == "200":
+                return True, response.json()
+            else:
+                return False, response.json()
+        else:
+            return False, "无法连接至服务器"
+
+
+class World:
+    @staticmethod
+    def progress(ip: str, port: str, token: str):
+        """
+        获取服务器的进度
+        :param ip: 服务器ip
+        :param port: 服务器端口
+        :param token: 服务器令牌
+        :return: 执行结果 成功返回[True, 执行结果] 失败返回[False, 执行结果/失败原因]
+        """
+        url = f"http://{ip}:{port}/world/progress?&token={token}"
+        headers = {"Accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            if response.json()["status"] == "200":
+                return True, response.json()
+            else:
+                return False, response.json()
+        else:
+            return False, "无法连接至服务器"
